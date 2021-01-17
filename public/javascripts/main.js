c9d0b8e13e5bac592122ffb70a4a256b6038cd8d
Leobardo Arguelles <leobardomed@gmail.com>
2021-01-16 18:46:27 -0800

    Fix tailwind bug

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,14 @@
+const xhr = new XMLHttpRequest();
 var socket = io();
+
+let form = document.getElementById("suggestion");
+let input = document.getElementById("input");
+
+form.addEventListener('submit', function(e) {
+  // e.preventDefault();
+  if (input.value) {
+    // xhr.open("GET", )
+    socket.emit('Searching for: ', input.value.trim());
+    input.value = '';
+  }
+});
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+require('dotenv').config();
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../views/html/', 'index.html'));
+});
+
+// POST suggestion to home page.
+router.post('/', function(req, res) {
+  console.log('POST');
 });
 
 module.exports = router;
